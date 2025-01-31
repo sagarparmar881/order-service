982ab18fd94db181d8e2c67087c5236829bb3b25
Sagar Parmar <sagarparmar881@gmail.com>
2025-01-31 09:42:30 +0530

    feat :: order-service :: Initial commit.

--- src/main/java/com/sagar/microservices/order/OrderServiceApplication.java
+++ src/main/java/com/sagar/microservices/order/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.sagar.microservices.order_service;
+package com.sagar.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
