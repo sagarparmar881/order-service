9d8e6bc26f6427bb3dabaeb8852049f4eb44f801
Sagar Parmar <sagarparmar881@gmail.com>
2025-11-01 18:53:34 +0530

    feat :: order-service :: Updated port number.

--- src/main/java/com/sagar/microservices/order/client/InventoryClient.java
+++ src/main/java/com/sagar/microservices/order/client/InventoryClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "inventory", url = "http://localhost:8083")
+@FeignClient(value = "inventory", url = "http://localhost:8082")
 public interface InventoryClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
